a611e37fe9514c988c4721d8e0c30a006eaff705
p22mbonilla <p22mbonilla@fairfieldprep.org>
2022-04-28 21:52:08 -0400

    complete 2

--- lab17_3.py
+++ lab17_3.py
@@ -1,1 +1,14 @@
 # Author MB 04/12/2022
+class TV_remote:
+
+    def __init__(self):
+        """ Initializing values """
+        self.channel = 0
+        self.volume = 0
+        self.On = False
+    
+    def to_string(self):
+        return "Channel:",self.channel,"Volume:",self.volume,"On:",self.On
+
+tv = TV_remote()
+print(tv.to_string())
